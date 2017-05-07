2aef9d6f99c0e7f7b98aacdaab034d0c017ca303
Geeprox <geeprox.me+github@gmail.com>
2017-05-07 11:27:26 +0800

    Add basic handlers(add/show/done etc.)

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ VERSION = '1.0'
 
 class CommandParser():
 
-    def __init__(self):
+    def __init__(self, application='', description='', epilog=''):
         self.command_map = defaultdict(list)
         self.help_map = {}
 
         self.default_error_handlers = [self._command_not_found_handler]
-        self.default_error_message = 'Undefined command called'
+        self.default_error_message = 'Undefined command({}) called!'
+
+        self._application = application
+        self._description = description
+        self._epilog = epilog
 
     def _verify_command_type(self, command):
         if not isinstance(command, str):
@@ -36,7 +40,8 @@ class CommandParser():
 
         for command in commands:
             self.command_map[command].extend(handlers)
-            self.help_map[command] = str(help_message)
+
+        self.help_map['/'.join(commands)] = str(help_message)
 
     def parse(self, items):
         try:
@@ -74,41 +79,39 @@ class CommandParser():
         if arg_cache:
             args[arg_cache] = True
 
+        # print(command, payload, args)
         return self._excute(command, payload, args)
 
     def _excute(self, command, payload, args):
-        try:
-            handlers = self.command_map[command]
-        except KeyError:
+        if command not in self.command_map:
+            self._command_not_found_handler(command)
             return False
 
-        for handler in handlers:
+        for handler in self.command_map[command]:
             handler(payload, args)
 
         return True
 
-    def add_hook(self, command, handler):
-        try:
-            self._verify_command_type(command)
-
-            if command not in self.command_map:
-                raise ValueError(
-                    'Command({}) should been added'.format(command))
-
-            self._verify_handler_type(handler)
-        except (TypeError, ValueError):
-            return False
-
-        self.command_map[command].insert(0, handler)
-        return True
-
-    def _command_not_found_handler(self):
-        raise ValueError(self.default_error_message)
+    def _command_not_found_handler(self, error_command):
+        print(self.default_error_message.format(error_command))
 
     def print_help(self):
-        help_message = 'Help'
-        # TODO: Build help message
-        print(help_message)
+        help_message = []
+        help_message.append('usage: {}'.format(self._application))
+        for command in self.help_map:
+            help_message.append(' [{}]'.format(command))
+        help_message.append('\n\n')
+        help_message.append(self._description)
+
+        for command, command_help in self.help_map.items():
+            if command_help:
+                help_message.append('{}: {}\n'.format(command, command_help))
+
+        if self._epilog:
+            help_message.append('\n')
+            help_message.append(self._epilog)
+
+        print(''.join(help_message))
 
 
 class CommandHandler():
@@ -117,14 +120,56 @@ class CommandHandler():
         self._init_parser()
 
     def _init_parser(self):
-        self.command_parser = CommandParser()
+        self.command_parser = CommandParser('To.do')
         self.command_parser.add_arguement(
-            '-h', '--help', handlers=[self._help])
+            '-h', '--help',
+            handlers=[self._help],
+            help_message='Show this help')
         self.command_parser.add_arguement(
-            '-v', '--version', handlers=[self._version])
+            '-v', '--version',
+            handlers=[self._version],
+            help_message='Show the version')
+        self.command_parser.add_arguement(
+            'add', '+',
+            handlers=[self._add],
+            help_message='''Add an todo tasks
+    add [task_name] ... [-before/-b date] [-p priority] [-r Remarks]
+    Required parameters
+        [task_name]: This todo tasks's name
+    Optional parameters
+        [-before/-b date]: 'date' should be an legal date time, You will receive a reminder by the deadline
+        [-p priority]: 'priority' should be an integer between 1 and 5
+        [-r Remarks]: Add an remarks for this todo tasks''')
+        self.command_parser.add_arguement(
+            'today',
+            handlers=[self._today],
+            help_message='''Show the tasks you have to do today
+        ''')
+        self.command_parser.add_arguement(
+            'tomorrow',
+            handlers=[self._tomorrow],
+            help_message='''Show the tasks you have to do tomorrow
+        ''')
+        self.command_parser.add_arguement(
+            'show', 'list',
+            handlers=[self._show],
+            help_message='''Show all the tasks you have to do
+        ''')
+        self.command_parser.add_arguement(
+            'done', 'finished', '*',
+            handlers=[self._done],
+            help_message='''Marked that you have completed this task
+        ''')
+        self.command_parser.add_arguement(
+            'delete', 'del', '-',
+            handlers=[self._delete],
+            help_message='''Delete the task
+        ''')
 
     def deal_with(self, string):
-        self.command_parser.parse(string)
+        ok = self.command_parser.parse(string)
+        if not ok:
+            self._help()
 
     def _help(self, *kwargs):
         self.command_parser.print_help()
@@ -132,14 +177,29 @@ class CommandHandler():
     def _version(self, *kwargs):
         print('Version:', VERSION)
 
-    def add(self, args):
-        print('add called')
+    def _add(self, *kwargs):
+        pass
+        # TODO: Add
 
-    def today(self, args):
-        print('Today:')
+    def _today(self, *kwargs):
+        pass
+        # TODO: Today
 
-    def tomorrow(self):
-        print('Tomorrow')
+    def _tomorrow(self, *kwargs):
+        pass
+        # TODO: Tomorrow
+
+    def _show(self, *kwargs):
+        pass
+        # TODO: Show
+
+    def _done(self, *kwargs):
+        pass
+        # TODO: Done
+
+    def _delete(self, *kwargs):
+        pass
+        # TODO: Delete
 
 
 def main():
